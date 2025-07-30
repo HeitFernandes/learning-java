be55c0b7954737c00051fd1610a65b29125e5d24
Heitor <fernandesheitort2@gmail.com>
2025-07-30 15:45:30 -0300

    Implementando Records

--- src/Records/NinjaRecord.java
+++ src/Records/NinjaRecord.java
@@ -5,7 +5,7 @@ package Records;
 // TODO -  Records nao tem SETTER
 // TODO - Acessando os atributos deparamos que a classe record nao tem getNome é apenas Nome sem o "get" de inicio.
 // TODO - Quando usamos Records e tambem Enums?? Quando sabemos que algo sera constante, o valor nunca sera alterado.
-public record NinjaRecord(String nomeD, String email, int telefone) {
+public record NinjaRecord(String nome, String email, int telefone) {
 
     public String emailCaixaAlta() {
         return email.toUpperCase();
